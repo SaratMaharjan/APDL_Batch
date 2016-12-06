7c16dc4be3b781a3d57ac4c925c513d936d90983
saratmaharjan <sarat.maharjan@gmail.com>
2016-12-06 08:22:31 +0100

    m

--- SM.bat
+++ SM.bat
@@ -10,7 +10,7 @@ REM anfang Selecting Licenses and Processors
 	REM set appPath="C:\Program Files\ANSYS Inc\v162\ANSYS\bin\winx64\ansys162.exe"
 	set appPath="C:\Program Files\ANSYS Inc\v172\ANSYS\bin\winx64\ansys172.exe"
 	REM	1.Structural/ANSYS	2.Mech_2	3.Stba/MEBA	4.Prepost
-		set startValue=1
+		set startValue=2
 		REM set startValue=4
 		set endValue=!startValue!
 		REM set endValue=4
@@ -21,8 +21,8 @@ REM ende
 
 REM for /l %%z in ( 41 1 48 ) do (
 REM for %%z in ( 10 ) do (
-for %%z in ( 10 9 ) do (
-REM for %%z in ( 99 ) do (
+for %%z in ( 4 5 ) do (
+REM for %%z in ( 100 ) do (
 
 	call initials.bat
 	REM anfang Projekte (Active: 1-50)
@@ -32,15 +32,15 @@ REM for %%z in ( 99 ) do (
 	if %%z equ 4 (
 		REM goto :takeTime
 		set proj_Num=304516
-		set interval=2009 10 2099
-		set interval2=11 1 12
+		set interval=2119 10 2119
+		set interval2=21 1 21
 
 		REM set append=1
 		REM set pathBefore=D:
 		REM set pathAfter=ANSYSec
-		REM set redirect=1
+		set redirect=1
 		REM set execute=1
-		REM set extVar=9
+		set extVar=0
 		REM set sourcePre=R:\maharjan
 	)
 	REM REM ende
@@ -49,15 +49,15 @@ REM for %%z in ( 99 ) do (
 	if %%z equ 5 (
 		REM goto :takeTime
 		set proj_Num=304516
-		set interval=2119 10 2199
-		set interval2=11 1 12
+		set interval=2149 10 2149
+		set interval2=12 1 12
 
 		REM set append=1
 		REM set pathBefore=D:
 		REM set pathAfter=ANSYSec
-		REM set redirect=1
+		set redirect=1
 		REM set execute=1
-		REM set extVar=9
+		set extVar=0
 		REM set sourcePre=R:\maharjan
 	)
 	REM REM ende
@@ -154,8 +154,8 @@ REM for %%z in ( 99 ) do (
 	if %%z equ 100 (
 		REM goto :takeTime
 		set proj_Num=xxxxx
-		set interval=2029 10 2099
-		set interval2=11 1 12
+		set interval=2002 10 2002
+		set interval2=41 10 51
 
 		REM set append=1
 		REM set pathBefore=D:
