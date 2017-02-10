76af180b6289ac7a2dc1653aee0c43dfe1a5fee5
saratmaharjan <sarat.maharjan@gmail.com>
2017-02-10 18:11:08 +0100

    m

--- SM.bat
+++ SM.bat
@@ -10,16 +10,16 @@ REM anfang Selecting Licenses and Processors
 	set appPath="C:\Program Files\ANSYS Inc\v180\ANSYS\bin\winx64\ansys180.exe"
 	REM set appPath="C:\Program Files\ANSYS Inc\v172\ANSYS\bin\winx64\ansys172.exe"
 	REM	1.Structural/ANSYS	2.Mech_2	3.Stba/MEBA	4.Prepost
-		set startValue=2
+		set startValue=4
 		REM set startValue=4
 		set endValue=!startValue!
 		REM set endValue=4
 	REM Number of Processors
-		set numPro=1
+		set numPro=2
 		REM set numPro=4
 REM ende
 
-for %%z in ( 3 ) do (
+for %%z in ( 9 ) do (
 REM for %%z in ( 100 ) do (
 
 	call initials.bat
@@ -64,7 +64,7 @@ REM for %%z in ( 100 ) do (
 	if %%z equ 3 (
 		REM goto :takeTime
 		set proj_Num=308817
-		set interval=1000 10 1000
+		set interval=1010 10 1050
 		set interval2=4 1 4
 
 		REM set append=1
@@ -161,6 +161,25 @@ REM for %%z in ( 100 ) do (
 		REM set sourcePre=R:\maharjan
 	)
 	REM REM ende
+
+	REM REM anfang 9 - 309317 - S-1078 Schneckenförderer
+	if %%z equ 9 (
+		REM goto :takeTime
+		set proj_Num=309317
+		set interval=0 10 0
+		set interval2=1 1 1
+
+		REM set append=1
+		REM set pathBefore=D:
+		REM set pathAfter=ANSYSec
+		REM set redirect=1
+		REM set execute=1
+		REM set extVar=0
+		REM set sourcePre=R:\maharjan
+	)
+	REM REM ende
+
+
 
 	REM 99 anfang TEMPORARY CALCULATIONS §§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§
 	if %%z equ 99 (
