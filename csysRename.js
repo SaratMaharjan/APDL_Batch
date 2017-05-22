2802511cacbc0e95e2eda424bc02220dd965c769
saratmaharjan <sarat.maharjan@gmail.com>
2017-05-22 08:19:51 +0200

    minor changes b4 0522

--- csysRename.js
+++ csysRename.js
@@ -5,12 +5,12 @@ function csys_Rename() {
 	//debugger;
 	var c4 = DS.Tree.FirstActiveBranch.CoordinateSystemGroup;
 	var c5 = c4.Children.Count;
-	for(var i = 2; i <= c5; i++) {
+	for(var i = 4; i <= c5; i++) {
 		//c4.Children.Item(2).CoordinateSystemType = SC.id_Cylindrical
 		var c6 = c4.Children.Item(i);
 		c6.Name = "csys_local" + (i-1);
 		c6.AnsysSystemAuto = 0;
-		c6.AnsysSystemNumber = 20 + i - 1;
+		c6.AnsysSystemNumber = 20 + i - 3;
 		c6.PrimaryAxis = 3;
 		//c6.PrimaryAxisAlignment = 4;
 		c6.PrimaryAxisAlignment = 1;
