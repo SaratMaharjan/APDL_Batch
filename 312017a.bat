68f3f44d3d69679e18f41bdc3df95acfb455b2ee
saratmaharjan <sarat.maharjan@gmail.com>
2017-06-28 14:24:43 +0200

    cleaned up

--- 312017a.bat
+++ 312017a.bat
@@ -2,7 +2,7 @@
 call reset.bat
 
 REM set pathBefore=E:
-REM set pathAfter=ANSYS
+REM set pathAfter=ansys
 
 REM set sourcePre=R:\maharjan
 REM set sourceFile=s.in
@@ -10,27 +10,27 @@ REM set sourceFile=s.in
 REM set redirect=0
 REM set extVar=0
 
-set proj_Num=312017
+set proj_Num=312017a
+
+REM +ve CS 1
+	set interval=8010 10 8010
+	set interval2=002 1 002
+		call calc.bat
 
 REM REM +ve CS 1
-REM 	set interval=1008 10 1008
+REM 	set interval=1018 10 1018
 REM 	set interval2=12 1 12
 REM 		call calc.bat
 
-REM REM +ve CS 1
-REM 	set interval=1018 20 1038
-REM 	set interval2=11 1 11
+REM REM +ve CS 2
+REM 	set interval=1038 10 1038
+REM 	set interval2=12 1 12
 REM 		call calc.bat
 
-REM +ve CS 2
-	set interval=1068 10 1068
-	set interval2=12 1 12
-		call calc.bat
-
-REM +ve CS 2
-	set interval=1168 10 1168
-	set interval2=12 1 12
-		call calc.bat
+REM REM +ve CS 2
+REM 	set interval=1138 10 1138
+REM 	set interval2=12 1 12
+REM 		call calc.bat
 
 REM REM +ve CS 1
 REM 	set interval=1088 10 1098
---
+++ server.js
@@ -0,0 +1,14 @@
+
+// very basic express server
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(__dirname));
+
+app.listen(process.argv[2], (err) => {
+	if (err) return console.log('cannot start server.');
+	console.log(`Listening on port ${process.argv[2]}`);
+});
