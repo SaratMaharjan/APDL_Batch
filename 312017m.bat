f399076b693f830fd6d3fd3df76d6646e979a26c
smhrjn <sarat.maharjan@gmail.com>
2017-07-26 17:17:06 +0200

    regular

--- 312017m.bat
+++ 312017m.bat
@@ -18,25 +18,26 @@ REM 	set interval2=11 1 11
 REM 		call calc.bat
 
 REM +ve CS 1
-	set interval=2009 20 2009
+	set interval=2019 20 2099
 	set interval2=11 10 21
 		call calc.bat
 
-REM REM set redirect=1
-REM REM +ve CS 2
-REM 	set interval=1008 10 1058
-REM 	set interval2=11 1 12
-REM 		call calc.bat
+REM set redirect=1
+REM +ve CS 2
+	set interval=2119 20 2199
+	set interval2=11 10 21
+		call calc.bat
 
-REM REM +ve CS 2
-REM 	set interval=1118 10 1158
-REM 	set interval2=11 1 12
-REM 		call calc.bat
+REM +ve CS 1
+	set interval=2029 20 2089
+	set interval2=11 10 21
+		call calc.bat
 
-REM REM +ve CS 1
-REM 	set interval=1068 10 1098
-REM 	set interval2=11 1 12
-REM 		call calc.bat
+REM set redirect=1
+REM +ve CS 2
+	set interval=2129 20 2189
+	set interval2=11 10 21
+		call calc.bat
 
 REM REM +ve CS 1
 REM 	set interval=1178 10 1198
