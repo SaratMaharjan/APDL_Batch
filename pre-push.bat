191169a010eedf196e86eb127241fcb405860d62
smhrjn <sarat.maharjan@gmail.com>
2017-11-14 16:19:14 +0100

    'regular'

--- pre-push.bat
+++ pre-push.bat
@@ -1,8 +1,8 @@
 copy C:\\Users\\Maharjan\\AppData\\Local\\SpaceClaim\\CustomShortcuts.xml D:\\zcore\\CustomShortcuts.xml^
-&& copy C:\\Users\\Maharjan\\.gitconfig D:\\zcore\\.gitconfig^
-&& copy C:\\Users\\Maharjan\\AppData\\Local\\SpaceClaim\\SpaceClaim.exe_Url_mxknckiray12wbp2nbjrw2dmbpybo0of\\1.0.0.0\\user.config D:\\zcore\\user.config^
-&& copy C:\\Users\\Maharjan\\AppData\\Roaming\\Microsoft\\Templates\\Normal.dotm D:\\zcore\\Normal.dotm^
-&& npm list --global --depth=0 > D:\zCore\npm-list.txt^
-&& type D:\zCore\npm-list.txt^
-&& pip list > D:\zCore\pip-list.txt^
-&& type D:\zCore\pip-list.txt
+ && copy C:\\Users\\Maharjan\\.gitconfig D:\\zcore\\.gitconfig^
+ && copy C:\\Users\\Maharjan\\AppData\\Local\\SpaceClaim\\SpaceClaim.exe_Url_mxknckiray12wbp2nbjrw2dmbpybo0of\\1.0.0.0\\user.config D:\\zcore\\user.config^
+ && copy C:\\Users\\Maharjan\\AppData\\Roaming\\Microsoft\\Templates\\Normal.dotm D:\\zcore\\Normal.dotm^
+ && npm list --global --depth=0 > D:\zCore\npm-list.txt^
+ && type D:\zCore\npm-list.txt^
+ && pip list > D:\zCore\pip-list.txt^
+ && type D:\zCore\pip-list.txt
