3099e0514a93167a5230e594a229f5250c7e50c2
smhrjn <sarat.maharjan@gmail.com>
2018-01-13 14:45:48 +0100

    git-push updated

--- pre-push.bat
+++ pre-push.bat
@@ -1,6 +1,7 @@
 copy C:\\Users\\Maharjan\\AppData\\Local\\SpaceClaim\\CustomShortcuts.xml D:\\zcore\\CustomShortcuts.xml^
  && copy C:\\Users\\Maharjan\\.gitconfig D:\\zcore\\.gitconfig^
  && copy C:\\Users\\Maharjan\\AppData\\Local\\SpaceClaim\\SpaceClaim.exe_Url_mxknckiray12wbp2nbjrw2dmbpybo0of\\1.0.0.0\\user.config D:\\zcore\\user.config^
+ && copy C:\\Users\\maharjan\\AppData\\Local\\SpaceClaim\\SpaceClaim\\barlayout2.xml D:\\zcore\\barlayout2.xml^
  && copy C:\\Users\\Maharjan\\AppData\\Roaming\\Microsoft\\Templates\\Normal.dotm D:\\zcore\\Normal.dotm^
  && npm list --global --depth=0 > D:\zCore\npm-list.txt^
  && type D:\zCore\npm-list.txt^
