0336e5e42860247354dca71182a7ef9ef1b7dda5
smhrjn <sarat.maharjan@gmail.com>
2018-09-27 19:15:01 +0200

    'regular'

--- pre-push.bat
+++ pre-push.bat
@@ -1,8 +1,20 @@
-copy C:\\Users\\Maharjan\\AppData\\Local\\SpaceClaim\\CustomShortcuts.xml z:\\CustomShortcuts.xml^
- && copy C:\\Users\\Maharjan\\.gitconfig z:\\.gitconfig^
- && copy C:\\Users\\Maharjan\\AppData\\Local\\SpaceClaim\\SpaceClaim.exe_Url_mxknckiray12wbp2nbjrw2dmbpybo0of\\1.0.0.0\\user.config z:\\user.config^
- && copy C:\\Users\\maharjan\\AppData\\Local\\SpaceClaim\\SpaceClaim\\barlayout2.xml z:\\barlayout2.xml^
- && copy C:\\Users\\Maharjan\\AppData\\Roaming\\Microsoft\\Templates\\Normal.dotm z:\\Normal.dotm^
+
+REM for ansys2
+REM copy C:\\Users\\Maharjan\\AppData\\Local\\SpaceClaim\\CustomShortcuts.xml z:\\CustomShortcuts.xml^
+REM  && copy C:\\Users\\Maharjan\\.gitconfig z:\\.gitconfig^
+REM  && copy C:\\Users\\Maharjan\\AppData\\Local\\SpaceClaim\\SpaceClaim.exe_Url_mxknckiray12wbp2nbjrw2dmbpybo0of\\1.0.0.0\\user.config z:\\user.config^
+REM  && copy C:\\Users\\maharjan\\AppData\\Local\\SpaceClaim\\SpaceClaim\\barlayout2.xml z:\\barlayout2.xml^
+REM  && copy C:\\Users\\Maharjan\\AppData\\Roaming\\Microsoft\\Templates\\Normal.dotm z:\\Normal.dotm^
+REM  && npm list --global --depth=0 > z:\npm-list.txt^
+REM  && type z:\npm-list.txt^
+REM  && pip list > z:\pip-list.txt^
+REM  && type z:\pip-list.txt
+
+copy C:\\Users\\Asus\\AppData\\Local\\SpaceClaim\\CustomShortcuts.xml z:\\CustomShortcuts.xml^
+ && copy C:\\Users\\Asus\\.gitconfig z:\\.gitconfig^
+ && copy C:\\Users\\Asus\\AppData\\Local\\SpaceClaim\\SpaceClaim.exe_Url_kjk2gifgxagupbz2gt1u0hx4plfqiaxf\\1.0.0.0\\user.config z:\\user.config^
+ && copy C:\\Users\\Asus\\AppData\\Local\\SpaceClaim\\SpaceClaim\\barlayout2.xml z:\\barlayout2.xml^
+ && copy C:\\Users\\Asus\\AppData\\Roaming\\Microsoft\\Templates\\Normal.dotm z:\\Normal.dotm^
  && npm list --global --depth=0 > z:\npm-list.txt^
  && type z:\npm-list.txt^
  && pip list > z:\pip-list.txt^
